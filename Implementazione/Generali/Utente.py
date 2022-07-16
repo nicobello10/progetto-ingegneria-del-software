12f69cfc2ee6e32866f7b46911a402b78cb12ef8
ivanpacenti <s1103326@studenti.univpm.it>
2022-07-16 10:44:25 +0200

    aggiunte classi utente e relativo gestore

---
+++ Implementazione/Generali/Utente.py
@@ -0,0 +1,11 @@
+class Utente():
+    def __init__(self, nome, cognome, dataNascita, numeroCell, password,nomeUtente):
+        self.nomeUtente = nomeUtente
+        self.password = password
+        self.nome=nome
+        self.cognome=cognome
+        self.dataNascita=dataNascita
+       # _ID=_ID+1
+        self.numeroCell=numeroCell
+        
+    
--- Implementazione/Gestione/Gestione_utente/gestione_utente.py
+++
@@ -1,3 +0,0 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow
-from PyQt5 import uic
-

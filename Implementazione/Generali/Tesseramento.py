7987659e6667b92f159486852f028c20f202229d
ivanpacenti <s1103326@studenti.univpm.it>
2022-07-18 23:11:41 +0200

    aggiunta possibilità di modificare utenti per admin

--- Implementazione/Generali/Tesseramento.py
+++ Implementazione/Generali/Tesseramento.py
@@ -1,8 +1,12 @@
+from datetime import date
+
+
 class Tesseramento():
     def __init__(self, email,codiceFiscale,tipoTesseramento):
         self.email=email
         self.codiceFiscale=codiceFiscale
         self.tipoTesseramento=tipoTesseramento
+        self.dataTesseramento=date.today()
 
 
     def getTipoTesseramento(self):

8ce87b95708734122c4c68f823135028577a23ef
nicopiccia <nicola.picciafuoco@gmail.com>
2022-07-19 18:23:49 +0200

    classe prenotazione

---
+++ Implementazione/Generali/Prenotazione.py
@@ -0,0 +1,34 @@
+
+
+
+class Prenotazione():
+
+    def __init__(self,campo,data,oraFine,oraInizio):
+        self.campo = campo
+        self.data = data
+        self.oraFine = oraFine
+        self.oraInzio = oraInizio
+
+    def getCampo(self):
+        return self.campo
+
+    def setCampo(self):
+        self.campo= campo
+
+    def getData(self):
+        return self.data
+
+    def setData(self):
+        self.data=data
+
+    def getOraFine(self):
+        return self.oraFine
+
+    def setOraFine(self):
+        self.oraFine=orafine
+
+    def setOraInizio(self):
+        self.oraInzio = oraInzio
+
+    def getOraInizio(self):
+        return self.oraInzio

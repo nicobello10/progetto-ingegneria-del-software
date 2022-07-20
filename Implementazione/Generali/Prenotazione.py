04f69e6941243068ca2909578dcb10e60803b739
nicopiccia <nicola.picciafuoco@gmail.com>
2022-07-20 17:48:49 +0200

    Prenotazione.py

---
+++ Implementazione/Generali/Prenotazione.py
@@ -0,0 +1,8 @@
+from Implementazione.Generali.Campo import Campo
+class Prenotazione():
+
+    def __init__(self,data,oraInizio,campo,utente):
+        self.data=data
+        self.oraInizio=oraInizio
+        self.campo=campo
+        self.utente=utente
